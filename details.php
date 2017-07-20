8fc1efa120ea03c5e177620bde6f8cf630cef1a9
jbreu <jakobbreu@googlemail.com>
2017-07-20 11:17:48 +0200

    Anzeige von Vorgänger- und Nachfolgerbünden

--- details.php
+++ details.php
@@ -33,10 +33,16 @@
 
 		echo "<h1>".$row->name."</h1><br/>";
 		echo "Ort: ".$row->ortname." (".$row->region.")<br/>";
-        	echo "Aktiv: ".($row->aktiv?"Ja":"Nein")."<br/>";
-        	echo "Gründung: ".$row->gtag."".$row->gzeitraum."<br/>";
-        	echo "Wahlspruch: ".$row->wahlspruch."<br/>";
-        	echo "Aufgegangen in: ".$row->fusion."<br/>";
+  	echo "Aktiv: ".($row->aktiv?"Ja":"Nein")."<br/>";
+  	echo "Gründung: ".$row->gtag."".$row->gzeitraum."<br/>";
+  	echo "Wahlspruch: ".$row->wahlspruch."<br/>";
+
+    if (!empty($row->fusion_id)) {
+      echo "Aufgegangen in: <a href='details.php?kid=".$row->fusion_id."'>".get_korporation($row->fusion_id)->name."</a><br/>";
+    } else if (!empty($row->fusion_text)) {
+      echo "Aufgegangen in: ".$row->fusion_text."<br/>";
+    }
+
 		echo "Verband: ".$row->verbandname."<br/><br/>";
 
     $band = get_band($row);
@@ -74,32 +80,85 @@
             echo "</div>";
 	     }
 
-       $sql = "SELECT name FROM korporation WHERE korporation.aufgegangenin_id=".$kid;
-
+       $sql = "SELECT * FROM ereignis WHERE korporation=".$kid;
        $statement = $mysqli->prepare($sql);
        $statement->execute();
 
-       $statement->store_result();
+       $result = $statement->store_result();
        $count = $statement->num_rows;
 
        if($count>0) {
-
          echo '<br />
-            In dieser Korporation aufgegangen:<br />
+Ereignisse:<br />
 
-            <table  class="table table-hover table-responsive">
-            	<tr class="active">
-            		<th>Name</th>
-            		<th>Ort</th>
-                <th>Aktiv</th>
-            		<!--<th>Region</th>-->
-            		<th>Gründung</th>
-            		<th>Wahlspruch</th>
-            		<!--<th>Aufgegangen in</th>-->
-            		<!--<th>Verband</th>-->
-            		<th>Farben</th>
-              	</tr>
-          ';
+<table id=ereignisse class="table table-hover table-responsive">
+<tbody>
+  <tr class="active">
+    <th>Zeitraum</th>
+    <th>Details</th>
+    <th>Quelle</th>
+  </tr>';
+
+  $sql = "SELECT ereignis.datum as datum, ereignis.jahr as jahr, ereignistyp.name as typname, ereignis.fremdeKorporation1 as fremdeKorporation1_id, korporation.name as fremdeKorporation1_name, verband.name as verbandname, korporationstyp.name as ktypname, quelle.id as quelleid, quelle.kuerzel as quellekuerzel FROM ereignis LEFT JOIN ereignistyp ON ereignis.type=ereignistyp.id LEFT JOIN korporation ON ereignis.fremdeKorporation1=korporation.id LEFT JOIN verband ON ereignis.verband=verband.id LEFT JOIN korporationstyp ON ereignis.korporationstyp=korporationstyp.id LEFT JOIN quelle ON ereignis.quelle=quelle.id WHERE ereignis.korporation=".$kid;
+
+  $statement = $mysqli->prepare($sql);
+  $statement->execute();
+
+  $result = $statement->get_result();
+
+    while($row = $result->fetch_object()) {
+      echo "<tr><td>".$row->datum.$row->jahr."</td><td>".$row->typname.": ".$row->text;
+
+      if (!empty($row->fremdeKorporation1_id)) {
+        echo "<a href='details.php?kid=".$row->fremdeKorporation1_id."'>".$row->fremdeKorporation1_name."</a>";
+      }
+
+      if (!empty($row->verbandname)) {
+        echo "<a>".$row->verbandname."</a>";
+      }
+
+      if (!empty($row->ktypname)) {
+        echo "<a>".$row->ktypname."</a>";
+      }
+
+      if (!empty($row->quellekuerzel) && !empty($row->quelleid)) {
+        echo "</td><td><a href='quellen.php#anker".$row->quelleid."'>".$row->quellekuerzel."</a></td></tr>";
+      } else {
+        echo "</td><td></td></tr>";
+      }
+    }
+
+    echo '</tbody>
+        </table>';
+  }
+
+
+   $sql = "SELECT name FROM korporation WHERE korporation.aufgegangenin_id=".$kid;
+
+   $statement = $mysqli->prepare($sql);
+   $statement->execute();
+
+   $statement->store_result();
+   $count = $statement->num_rows;
+
+   if($count>0) {
+
+     echo '<br />
+        In dieser Korporation aufgegangen:<br />
+
+        <table  class="table table-hover table-responsive">
+        	<tr class="active">
+        		<th>Name</th>
+        		<th>Ort</th>
+            <th>Aktiv</th>
+        		<!--<th>Region</th>-->
+        		<th>Gründung</th>
+        		<th>Wahlspruch</th>
+        		<!--<th>Aufgegangen in</th>-->
+        		<!--<th>Verband</th>-->
+        		<th>Farben</th>
+          	</tr>
+      ';
 
 
   	$sql = "SELECT korporation.id as kid, korporation.name as name, ort.name as ortname, ort.region as region, korporation.aktiv as aktiv, korporation.gruendungstag as gtag, korporation.gruendungszeitraum as gzeitraum, korporation.wahlspruch as wahlspruch, korporation.aufgegangenin_text as fusion, verband.name as verbandname, band.farbe1 as farbe1, band.farbe2 as farbe2, band.farbe3 as farbe3, band.farbe4 as farbe4, band.farbe5 as farbe5 FROM korporation LEFT JOIN ort ON korporation.ort=ort.id LEFT JOIN verband on korporation.verband=verband.id LEFT JOIN band on band.korporation=korporation.id WHERE korporation.aufgegangenin_id=".$kid;
