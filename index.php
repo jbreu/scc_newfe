f6e3d102a435099e0d9fb19f69e7ec8176f248d0
jbreu <jakobbreu@googlemail.com>
2017-07-20 11:04:43 +0200

    Quellenverzeichnis im Index

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php include 'header.php';?>
 
 <a href="table.php?aktiv=on">Tabellenansicht</a><br />
-<a href=couleursuche.php>Couleursuche</a>
+<a href="couleursuche.php">Couleursuche</a><br />
+<a href="quellen.php">Quellen</a>
 
 <?php include 'footer.php' ?>
